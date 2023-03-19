f30637c7dfed65fc535ec83ac7140a92b0e4f3f3
RyotaHiyama <ryotahiyama@ca.info.hiroshima-cu.ac.jp>
2023-03-20 02:08:24 +0900

    update：chip処理の準備、別PC作業引継ぎpush

--- app/src/main/java/com/nokopi/activitylaunchmodetest/ActivityA.kt
+++ app/src/main/java/com/nokopi/activitylaunchmodetest/ActivityA.kt
@@ -2,10 +2,18 @@ package com.nokopi.activitylaunchmodetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nokopi.activitylaunchmodetest.databinding.ActivityABinding
+import androidx.databinding.DataBindingUtil
 
 class ActivityA : AppCompatActivity() {
+    private lateinit var binding: ActivityABinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_a)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_a)
+
+        binding.singleTopChip.setOnCloseIconClickListener {
+
+        }
+
     }
 }
